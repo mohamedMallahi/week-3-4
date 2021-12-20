48cab0b36b0298400088aec97665a52af2c7cd17
mohamedMallahi <kingcoding17@gmail.com>
2021-12-20 20:04:11 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const a = 3
+const RemixFramework = {
+  name: 'Remix',
+  built: {
+    name: 'Next',
+    built: {
+      name: 'React',
+      built: {
+        name: 'Javascript',
+      },
+    },
+  },
+};
 
-function print() {
-  console.log(3)
+const DjangoFramework = {
+  name: 'Django',
+  built: {
+    name: 'Python',
+    built: {
+      name: 'C++',
+      built: {
+        name: 'C',
+        built: {name: 'Jack'}
+      }
+    }
+  }
 }
+
+function countDown(num) {
+  if (num <= 0) {
+    console.log('Hooray');
+    return 'end';
+  }
+  console.log(num);
+  countDown(num - 1);
+}
+
+// countDown(10);
+
+function sumRange(num, total = 0) {
+  if (num <= 0) {
+    return total;
+  }
+
+  return sumRange(num - 1, total + num);
+}
+
+function printFramework(framework) {
+  if (framework.built === null || framework.built === undefined) {
+    console.log(framework.name)
+    return 'Nothing';
+  }
+
+  console.log(framework.name);
+  return printFramework(framework.built);
+}
+
+// printFramework(RemixFramework);
+printFramework(DjangoFramework);
